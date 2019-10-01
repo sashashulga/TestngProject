84d1a81b0980e4413a51fdacc22a617d5a64ea11
sashashulga <alessandro587@yandex.ru>
2019-10-01 19:39:16 +0300

    Add Assert

--- src/HomePageTests.java
+++ src/HomePageTests.java
@@ -2,6 +2,7 @@ package tests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
@@ -14,9 +15,14 @@ public class HomePageTests extends tests.TestBase {
         WebElement listEvent
                 = driver.findElement(By.id("idtitletypesearchevents"));
         //--Verify that listEvent elements contains 'list events' text
-        System.out.println("Home Page Verification:"
-                + listEvent.getText().equals("List events"));
-        //System.out.println(listEvent.getAttribute("class"));
+        Assert.assertEquals(listEvent.getText(),"List events",
+                "Name of the listEventt element is not 'List evvents'");
+
+
+
+//        System.out.println("Home Page Verification:"
+//                + listEvent.getText().equals("List events"));
+//        //System.out.println(listEvent.getAttribute("class"));
 
     }
 
